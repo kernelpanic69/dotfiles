5c31d744ecf475406e86e33d4b9f42c0280f8c7b
John Doe <dev_stuff.2fcvp@aleeas.com>
2022-03-22 08:46:46 +0200

    Added dwm, updated waybar

--- .zshrc
+++ .zshrc
@@ -32,6 +32,8 @@ RPROMPT='%F{blue}%T %(?.%F{green}[%?].%F{red}[%?]%f)'
 
 source /usr/share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh 
 
+bindkey -v
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- install.sh
+++ install.sh
@@ -1,16 +1,25 @@
-ROOT="$(pwd)"
+#!/usr/bin/zsh
+
+ROOT="$(dirname $(readlink -f $0))"
 CONFIG=$HOME/.config/
 
 function install_zshrc {
   cp $ROOT/.zshrc $HOME/
+  . $HOME/.zshrc
 }
 
 function install_sway {
   cp -r $ROOT/sway $CONFIG
+  swaymsg reload
 }
 
 function install_waybar {
   cp -r $ROOT/waybar $CONFIG
+  swaymsg reload
+}
+
+function install_scripts() {
+  cp $ROOT/scripts/volume.sh $HOME/.local/bin/vol
 }
 
 function main {
@@ -29,8 +38,11 @@ function main {
     "waybar")
       install_waybar
     ;;
+    "scripts")
+	install_scripts
+    ;;
     *)
-      echo "Usage: $0 <all|zsh|sway|waybar>"
+      echo "Usage: $0 <all|zsh|sway|waybar|scripts>"
       exit 1
   esac
 }
